43ac46d88e70edb57576c5609c2c943de39ba5af
mercethem <mercethem@gmail.com>
2024-03-02 18:36:50 +0300

    initial commit

---
+++ pointer_and_reference/main.cpp
@@ -0,0 +1,12 @@
+/**
+
+Pointers and references almost same things. But if you want to const with ref just one difference
+that difference is for top level const because references are const already do not use const keyword
+for low level const!
+
+ATTENTION: Other rules are same!
+
+Pointers can be nullable (NULL in C nullptr in C++) but references can not be nullable
+Pointers are re-bindable but references are not re-bindable
+Can be a Pointer array but can not be a reference array
+ */
