abfd5d220223c007a99555dc877c4574e4c21921
mercethem <mercethem@gmail.com>
2024-03-04 05:22:46 +0300

    documentation updated and instance initialized

--- pointer_and_reference/main.cpp
+++ pointer_and_reference/main.cpp
@@ -1,12 +1,28 @@
 /**
 
-Pointers and references almost same things. But if you want to const with ref just one difference
-that difference is for top level const because references are const already do not use const keyword
-for low level const!
+    Pointers and references almost same things. But if you want to const with ref just one difference
+ that difference is for top level const because references are const already do not use const keyword
+ for low level const!
 
-ATTENTION: Other rules are same!
+ ATTENTION: Other rules are same!
 
-Pointers can be nullable (NULL in C nullptr in C++) but references can not be nullable
-Pointers are re-bindable but references are not re-bindable
-Can be a Pointer array but can not be a reference array
+ Pointers can be nullable (NULL in C nullptr in C++) but references can not be nullable
+ Pointers are re-bindable but references are not re-bindable
+ Can be a Pointer array but can not be a reference array
+
  */
+
+
+#include <iostream>
+
+
+int main()
+{
+    int a[] = {10, 20, 30, 40, 50};
+    int *ptr{a};
+    int *&r{ptr};
+    ++r; // ++ ptr;
+    ++*r; // ++ *ptr;
+    for (auto val: a) //it is likewise strlen within for loop!
+        std::cout << val << " "; // 10 21 30 40 50
+}
