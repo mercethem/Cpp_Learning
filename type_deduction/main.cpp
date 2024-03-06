aa72a38406e0378056fe48f05eb104aa42f02300
asus <mercethem@gmail.com>
2024-03-06 09:16:56 +0300

    initial commit

---
+++ type_deduction/main.cpp
@@ -0,0 +1,9 @@
+/*
+    (deduce ===> deduction)
+    There is type deduction in C++ especially modern C++ that look the data type and find what is the data type what it is
+    -auto
+    -decltype
+    -decltype(auto)
+    -template
+    -lambda expression
+ */
