e3ce55f3a29e87ab179d0c3eef1716f5c44731ae
asus <mercethem@gmail.com>
2024-03-14 05:50:15 +0300

    upgrade to documentation

--- overloading/main.cpp
+++ overloading/main.cpp
@@ -46,9 +46,22 @@
  int func(double)
  int func(int*)===> The function have got 4 overloads
 
-
  int func(int);
  int func(int, int x = 5); ===> There are 2 function overloading
+
+ int void (int, int);
+ int void (int, ...); ===> There are 2 function overloading (variadic function is always be a different type)
+
+ int void (int []);
+ int void (int *); ===> There is not a function overloading
+    explain : int[] ===> [] is a pointer not a always array
+
+ int void (int[]);
+ int void (int [10]);
+ int void (int [5]);
+ int void (int *); ===> There is not a function overloading
+    explain : int[] ===> [] is a pointer not a always array so all of these are redeclaration
+
 
 ------------------------------------------------------------------------------------------------------------------------
  ATTENTION FOR FUNCTION OVERLOADING!!!!!!!!!!!!!!!!!!!!!!!!!!!!
@@ -60,10 +73,23 @@
  void func(const int x) {}; ===> There is not a function overloading!!! That is a redefinition but so SYNTAX ERROR
 
  void func(int*);
- void func(const int*); ===> There is a function overloading because that is a pointer!!!!
+ void func(const int*); ===> There is 2 function overloading because that is a pointer!!!!
+
+ void func(int &);
+ void func(const int &); ===> There is 2 function overloading because that is a reference!!!!
+
+ void func(int &); L value reference
+ void func(const int &); const L value reference
+ void func(int &&); R value reference ===> There are 3 function overloading because that is a reference!!!!
+
+ VVVVVVVVVVVVVVVVVV
+    vvvvvvvvvvv
+        vvvvv
+          v
 
  void func(int* p);
  void func(int* const p); ===> There is NOT a function overloading!!!!
+ TOP LEVEL CONST is always not a overloading
 
 ------------------------------------------------------------------------------------------------------------------------
 
@@ -72,24 +98,26 @@
  void func(int);
  void func(itype); ===> There is NOT function overloading that is a redeclaration!!!
 
+ TYPE ALIAS
+ typedef inf itype; // c/c++ ===> using itype = int; //modern c++
 
+ */
+using itype = int;
+using fptr = int (*) (int); //===> typdef int (*fptr)(int); //function pointer TYPE ALIAS
 
+/**
+ ATTENTION
+------------------------------------------------------------------------------------------------------------------------
+ void func (char);
+ void func (unsigned char);
+ void func (signed char); ===> There are 3 overloading, it not depends to compiler about overloading
 
+ these types are distinct types (char, signed char, unsigned char)
 
-
-
-
-
-
-
+------------------------------------------------------------------------------------------------------------------------
+ void func(int);
+ void func(int32_t); ===> it depends to compiler because int32_t is name of standard typedef
+ ------------------------------------------------------------------------------------------------------------------------
 
 
  */
-
-#include <iostream>
-
-int main ()
-{
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
-}
