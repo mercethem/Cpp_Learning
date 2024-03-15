b917bbb295f53919cb345c41c82f6342837f23d6
asus <mercethem@gmail.com>
2024-03-15 11:02:52 +0300

    documents and instances updated

--- function_overload_resolution/main.cpp
+++ function_overload_resolution/main.cpp
@@ -12,6 +12,54 @@
 
 
  !!!STEPS!!!
- 1) candidate functions ===> look and save names of overloadings
- */
+ 1) candidate functions ===> compiler looks and saves to names of overloading
+ 2) viable function     ===> You function calls ownself is that valid???? (Doesn't matter narrowing conversion!!!!!)
+                    Yes ===> viable function
+                    No  ===> not a viable function
+    RULES : a) Declaration of function operands are compatible
+            b) All arguments to any parameters can be valid conversion in Syntax Rules
+ 3)
 
+ */
+#include <iostream>
+
+int main ()
+{
+    {
+
+        void func (long double);
+        void func (char);
+        void func (int, int);
+        void func (int *);
+
+        //func calls to candidate functions
+    }
+
+    {
+        void func (long double);
+        void func (char);
+        //func(21); // long double and char is viable for that function call
+
+        void func (int *);      // 1
+        void func (void *);     // 2
+        void func (nullptr_t);  // 3
+
+        func (malloc (1000));
+        //not viable for 1 (there isn't implicit conversion void* to int*)
+        //Viable for 2
+        //Viable for 3 (there isn't implicit conversion void* to nullptr)
+    }
+
+    {
+        enum Color
+        {
+            White, Yelllow, Gray, Pink, Red, Brown, Magenta, Purple, Blue, Black
+        };
+        void func (int);
+        void func (Color);
+
+        func (White); //viable for BOTH because there is implicit conversion enum to int
+    }
+
+
+};
