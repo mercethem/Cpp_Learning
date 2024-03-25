be2fe920c83cdaf226ae01dbd96abf47f5ff7f2b
asus <mercethem@gmail.com>
2024-03-25 08:59:11 +0300

    document updated

--- const_member_function/main.cpp
+++ const_member_function/main.cpp
@@ -2,13 +2,18 @@
 
 /**
  We can not change non-static data members of objects
+
+ What is the const member function ? The answer is this function do not change to the object!
+ Result ===> The important thing is only object not members!!!!!!
+ And how can i understand which function will be const ? About PROBLEM DOMAIN!
+
  */
 
 class MyClass {
 public:
     void print(int) const;    //===> Accesser/getter function
     //void print(int, const MyClass*); there is invisible parameter
-
+    void foo()const;
     void func();           //===> Mutatar/setter function
     //void func(MyClass*); there is invisible parameter
     int x;
@@ -24,9 +29,15 @@ void MyClass::print(int) const
 
 }
 
+
 void MyClass::func()
 {
+    foo();
+}
 
+void MyClass::foo()const
+{
+    //func(); //SYNTAX ERROR because we can not call a non-const function in const function
 }
 
 int main()
