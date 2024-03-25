c23f80b573236c16a5d7ef5175af61ef88633240
asus <mercethem@gmail.com>
2024-03-25 05:20:17 +0300

    initial commit

---
+++ multiple_inclusion_guard/macro.h
@@ -0,0 +1,10 @@
+
+#ifndef MULTIPLE_INCLUSION_GUARD_MACRO_H
+#define MULTIPLE_INCLUSION_GUARD_MACRO_H
+
+/**
+ That is multiple inclusion guard macro! Standard
+ #pragma once macro is not a standard!!!
+ */
+
+#endif //MULTIPLE_INCLUSION_GUARD_MACRO_H
