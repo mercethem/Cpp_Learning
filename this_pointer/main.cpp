f1fda3d8bcf195b7df7ececd2ba689e923589e23
asus <mercethem@gmail.com>
2024-03-26 06:38:27 +0300

    comments and instances updated

--- this_pointer/main.cpp
+++ this_pointer/main.cpp
@@ -2,23 +2,69 @@
 
 /**
  Keyword
- This ===> a pointer
- We can use only in non-static member function about classes
+ This ===> a keyword and pointer
+ We can use for only in non-static member function about classes
+
+ 'this' points the object
+ 'this' PR value expression such as  ===>   &x this->x
+
+ if we use '*this' than we can use the object!
+ this  ===> pointer of object NOT THE OBJECT ONLY ADDRESS!!!
+ *this ===> access the objects THAT IS NOT A ADDRESS ONLY OBJECT ITSELF
+
+
  */
 
 
 class MyClass {
 public:
     void func();
+
+    void foo();
+
+private:
+    int m_x;
 };
 
 void MyClass::func()
 {
-    this->func();
+//    m_x = 10;
+//    MyClass::m_x = 10; //equal to above instance (m_x unqualified name but m_x is qualified name
+//                       // because of be an operand of resolution operator)
+//
+//    m_x = 10;
+//    this -> m_x = 10; //equal to above instance
+////----------------------------------------------------------------------------------------------------------------------
+//    func();
+//    this -> func(); //equal to above instance
+//
+//    func();
+//    MyClass::func(); //equal to above instance
+
+    std::cout << "this    : " << this << "\n";
+}
+
+void f1(MyClass *p);
+
+void f2(MyClass &p);
+
+void f3(MyClass);
+
+void MyClass::foo()
+{
+    f1(this);
+    f2(*this);
+    f3(*this);
 }
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    MyClass myClass;
+
+    std::cout << "myClass : " << &myClass << "\n";
+
+    myClass.func();
+
+
     return 0;
 }
