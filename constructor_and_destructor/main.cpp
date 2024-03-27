7c6e47bc8692ad8ebce66cc0bebc37ec0046e750
asus <mercethem@gmail.com>
2024-03-27 10:01:33 +0300

    initial commit

--- constructor_and_destructor/main.cpp
+++ constructor_and_destructor/main.cpp
@@ -1,7 +1,53 @@
 #include <iostream>
 
+class MyClass {
+public:
+    MyClass(); //===> CONSTRUCTOR! CAN NOT BE STATIC/ CAN NOT BE CONST
+    MyClass(int);
+
+    MyClass(double);
+
+    MyClass(int, int);
+
+    MyClass(int, double);
+
+    ~MyClass();
+};
+
+MyClass::MyClass()
+{
+    std::cout << "Address : " << this << "  - Constructor was called!" << "\n";
+}
+
+MyClass::~MyClass()
+{
+    std::cout << "Address : " << this << "  - Destructor was called!" << "\n";
+}
+
+MyClass myClass;
+/**
+ When we default initialize a object then default constructor call!!!
+*/
+
+
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+
+    /**
+     ATTENTION:
+     Default Constructor of myClass     ===> starts BEFORE main function
+     Destructor of myClass              ===> starts AFTER  main function
+
+     this in constructor and this in destructor are same because same object of 'this' !!!
+     this is a pointer and point same object in here
+
+     */
+
+    std::cout << "main started\n";
+
+    std::cout << "main finished\n";
+
+
     return 0;
 }
+
