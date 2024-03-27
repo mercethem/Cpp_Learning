3e217d63400ea8aa71ac8b5aa749820f31659fa7
asus <mercethem@gmail.com>
2024-03-27 09:42:36 +0300

    document and instances updated

--- constructor/main.cpp
+++ constructor/main.cpp
@@ -8,19 +8,59 @@
         for instance:
             class MyClass{
             public:
-                Myclass();   ===> There is no return concept, not even void!!!!
+                MyClass();   ===> There is no return concept, not even void!!!!
             };
  5) CAN NOT be a const
  6) be a public or a private or a protected ATTENTION Be careful to access control!!!
+ 7) A Class can have to lots of contracture with different signature result is Can do overload
+
+ ATTENTION : 1) If a constructor have not got any parameter that constructor name is DEFAULT CONSTRUCTOR
+             2)
+
  */
 
-class Myclass{
+class MyClass {
 public:
-    Myclass(); //===> CONSTRUCTOR! CAN NOT BE STATIC/ CAN NOT BE CONST
+    MyClass(); //===> CONSTRUCTOR! CAN NOT BE STATIC/ CAN NOT BE CONST
+    MyClass(int);
+
+    MyClass(double);
+
+    MyClass(int, int);
+
+    MyClass(int, double);
+
+    ~MyClass();
 };
+
+MyClass::MyClass()
+{
+    std::cout << "Address : " << this << "  - Constructor was called!" << "\n";
+}
+
+MyClass::~MyClass()
+{
+    std::cout << "Address : " << this << "  - Destructor was called!" << "\n";
+}
+
+MyClass myClass;
+    /**
+     When we default initialize a object then default constructor call!!!
+    */
+
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+
+    /**
+     ATTENTION:
+     default constructor of myClass start before main function
+     Destructor of myClass start after main function
+     */
+    std::cout << "main started\n";
+
+    std::cout << "main finished\n";
+
+
     return 0;
 }
---
+++ destructor/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+/**
+ 1) Name of destructor same as contructor but have a tilda (~) front of the name
+ 2) Can NOT be a global function
+ 3) Can NOT be a static member function of the class (non-static)
+ 4) Can NOT be a const member function of the class (non-const)
+ 4) There is not a return concept
+        for instance:
+            class MyClass{
+            public:
+                ~Myclass();   ===> There is no return concept, not even void!!!!
+            };
+ 5) Can be a function with public, private and protected access specifiers
+ 6) Can NOT be do overload!!!
+  */
+
+class MyClass{
+public:
+    MyClass();
+    ~MyClass();
+    //~MyClass(int);Syntax error because destructors can not be overload!
+private:
+};
