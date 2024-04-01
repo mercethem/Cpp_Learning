f194dbced210f90de9a76fdb0bc574326e47c057
asus <mercethem@gmail.com>
2024-04-01 09:40:09 +0300

    documentation and instances updated

--- constructor_initializer_list/main.cpp
+++ constructor_initializer_list/main.cpp
@@ -1,5 +1,13 @@
 /**
- constructor initializer list /member initializer list(MIL)
+ constructor initializer list /member initializer list(M.I.L.)
+ the M.I.L. is different from the initializer because
+ ===> when we use the initializer : constructor of local objects create with garbage values and initialize members
+                                    constructor of global objects create with default values and initialize members
+ ===> when we use the M.I.L.         : constructor of local or global objects create with initialized values
+
+ ATTENTION: M.I.L. more efficient than the initializer!!!!
+
+ ATTENTION: There isn't able to any constructor about class!!! But compiler create a constructor itself!
  */
 
 #include <iostream>
@@ -7,13 +15,21 @@
 class MyClass {
 public:
     MyClass();
+
     void print() const;
+
 private:
-    int m_a, m_b;
+    int m_a, m_b; //m_a created first then m_b!!!
 };
 
-MyClass::MyClass() : m_a{10}, m_b{20}
+//MyClass::MyClass() : m_a(10), m_b(20)
+MyClass::MyClass() : m_a{ 10 }, m_b{ 20 } //===> Constructor initializer list
 {
+
+    /*
+    m_a = 10; //===> initializer !!! Different above
+    m_b = 20;
+     */
     std::cout << "My Class Default Constructor\n";
 
 }
