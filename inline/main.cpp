68d3d089c574047cd98c2c4820f3825bd8f34037
asus <mercethem@gmail.com>
2024-04-02 09:24:16 +0300

    initial commit

---
+++ inline/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+/**
+   actually inline keyword is a wish tell to compiler "if possibly when this function calls do not use linker
+                                                        please code of compiled form copy where call in lines :) "
+    inline functions can be use maybe not that situation is on about compiler
+
+    ATTENTION ===> inline function definitions stay in header file!!!! That is not UB do not worry be happy :)
+
+ */
