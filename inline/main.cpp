bfdb408f855a97d53215042c13523ae24e2cfd01
asus <mercethem@gmail.com>
2024-04-03 06:13:31 +0300

    documentation updated

--- inline/main.cpp
+++ inline/main.cpp
@@ -1,9 +1,15 @@
-#include <iostream>
 /**
+   inline functions generally short and frequently referred books
+
    actually inline keyword is a wish tell to compiler "if possibly when this function calls do not use linker
                                                         please code of compiled form copy where call in lines :) "
     inline functions can be use maybe not that situation is on about compiler
 
     ATTENTION ===> inline function definitions stay in header file!!!! That is not UB do not worry be happy :)
 
+    One Definition Rule (ODR) ===> about "inline" functions
+
+    !!!!!Anyone can see all function definition and that situation is disadvantage here!!!!!
+
+    ATTENTION ===> The compiler use inline optimizing in RUNTIME
  */
