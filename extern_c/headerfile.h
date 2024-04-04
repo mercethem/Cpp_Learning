63bde5a714aebf4290788d4bf393cddcf2c719be
asus <mercethem@gmail.com>
2024-04-04 10:57:43 +0300

    header file added for instance

---
+++ extern_c/headerfile.h
@@ -0,0 +1,16 @@
+
+#ifndef HEADERFILE_H
+#define HEADERFILE_H
+
+#ifdef __cplusplus
+extern "C"{
+#endif
+
+void func(int);
+void func2(char);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif //HEADERFILE_H
