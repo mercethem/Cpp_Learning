21d950ba0cfa468409979863b2f2a49d8a2e6777
asus <mercethem@gmail.com>
2024-04-04 05:12:36 +0300

    documentation and instances updated

--- default_contructor/main.cpp
+++ default_contructor/main.cpp
@@ -6,7 +6,39 @@
   4) data members initialized with default values
  */
 
-class MyClass{
+#include <iostream>
+
+class MyClass2 {
 public:
-    MyClass() = default; //default constructor ===> compiler write constructor's code (public or private doesn't matter)
+    MyClass2() = default; //default constructor ===> compiler write constructor's code (public or private doesn't matter)
 };
+
+
+class MyClass {
+public:
+    void print() const;
+
+private:
+    int x, y, z;
+};
+
+void MyClass::print() const
+{
+    std::cout << x << y << z << "\n";
+}
+
+int main()
+{
+    MyClass x; //default constructor has initialized with  undetermined values
+    /**
+     ATTENTION: IF WE WRITE OURSELF THE DEFAULT CONSTRUCTOR INSIDE CLASS THEN OUR CONSTRUCTOR DO NOT INITIALIZE ANY VARIABLES
+     THEY STILL UNDETERMINED VALUE
+     */
+
+    MyClass x2{}; //default constructor has initialized with value initialize. The values are 0 because they are primitive types
+
+    x.print();
+    x2.print();
+
+
+}
