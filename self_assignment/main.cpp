84edec9dc5ecdda5d76a31f2235541f0f3d28352
asus <mercethem@gmail.com>
2024-04-29 08:39:15 +0300

    reformat

--- self_assignment/main.cpp
+++ self_assignment/main.cpp
@@ -6,9 +6,10 @@ int main()
     k = k; //That is NOT a SYNTAX ERROR that is a self assignment but that is WRONG!!!!
 
     int x = 100;
-    int* iptr = &x;
+    int * iptr = &x;
     int& iref = x;
 
     *iptr = iref; ///That instance is a self assignment
+
     return 0;
 }
