1145676aa3430dbbcafcdba65eddb7c674c64d71
asus <mercethem@gmail.com>
2024-05-03 10:45:45 +0300

    initial commit

--- copy_constructor_and_move_constructor_choose/main.cpp
+++ copy_constructor_and_move_constructor_choose/main.cpp
@@ -1,23 +1,43 @@
 #include <iostream>
 
 class Myclass {
-
+public:
+    Myclass()= default;
+    Myclass(const Myclass& r){
+        std::cout << "copy constructor\n";
+    }
+    Myclass(Myclass&& r ){
+        std::cout << "move constructor\n";
+    }
 };
 
-
-void func(Myclass&& x)
+void func(const Myclass& r)
 {
-    Myclass y(x); //copy constructor calls because of x is L value expr.
+    //..
+    Myclass x(r); //for copy
+}
+
+void func(Myclass&& r)
+{
+    //..
+    Myclass x(std::move(r)); //for stealing
+}
+
+void func2(Myclass&& x)
+{
+        Myclass y(x); //copy constructor calls because of x is L value expr.
 //    Myclass y(std::move(x)); //move constructor calls because of x is R value expr. because of move()!!!
 
-    Myclass m;
-    m = x; //copy assignment function calls
-    m = std::move(x); //move assignment function calls
-
+        Myclass m;
+//        m = x; //copy assignment function calls
+//        m = std::move(x); //move assignment function calls
 }
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    Myclass m;
+    func(m);
+    func(std::move(m));
+
     return 0;
 }
