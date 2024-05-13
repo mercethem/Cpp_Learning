ab924c58c935f7afab1e3ef3de80c53fad2a8e50
asus <mercethem@gmail.com>
2024-05-13 09:31:56 +0300

    instances and comments updated

---
+++ copy_elision/cmake-build-release/CMakeFiles/copy_elision.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/copy_elision.dir/main.cpp.o"
+  "CMakeFiles/copy_elision.dir/main.cpp.o.d"
+  "copy_elision"
+  "copy_elision.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/copy_elision.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- copy_elision/main.cpp
+++ copy_elision/main.cpp
@@ -12,16 +12,79 @@ class Myclass {
 public:
     Myclass();
 
+    Myclass(int x);
+
+    ~Myclass();
+
     Myclass(const Myclass&);
+
+    Myclass(Myclass&&);
+
 };
 
 Myclass::Myclass()
 {
-    std::cout << "Myclass()\n";
+    std::cout << "default constructor has called\n";
 }
+
+Myclass::Myclass(int x)
+{
+    std::cout << "constructor has called x = " << x << "\n";
+}
+
+Myclass::~Myclass()
+{
+    std::cout << "destructor has called\n";
+}
+
 
 Myclass::Myclass(const Myclass&)
 {
-    std::cout << "copy constructor()\n";
+    std::cout << "copy constructor has called\n";
+}
+
+Myclass::Myclass(Myclass&&)
+{
+    std::cout << "move constructor has called\n";
+}
+
+
+void func(Myclass x)
+{
+    std::cout << "func has called\n";
+}
+
+Myclass foo()
+{
+    return Myclass();
+}
+
+Myclass foo2()
+{
+
+    Myclass mx;
+    return mx;
+}
+
+int main()
+{
+    Myclass mx;
+    func(mx); //Two times destructor calls because of : object of mx and parameter mx
+
+
+    // ==> Mandatory copy elision
+    func(Myclass()); // Only one time destructor calls because of : only there is temporary object
+    //func(Myclass(10)); //same result above
+    Myclass x = Myclass{ }; //same result above
+
+    Myclass z = foo(); //==> Mandatory copy elision!!! Before 2017 ==> RVO Return value optimisation
+
+    /**
+     ATTENTION: there is move constructor there is or not for mandatory copy elision!!!
+
+     */
+    foo2(); // ==> NRVO Named return value optimisation ATTENTION : That is not mandatory copy elision
+
+    return 0;
 }
 
