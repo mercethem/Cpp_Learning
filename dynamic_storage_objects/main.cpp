6d61f099168ca4bac05344936f5971c9b2f96bff
asus <mercethem@gmail.com>
2024-05-15 07:31:49 +0300

    initial commit

--- dynamic_storage_objects/main.cpp
+++ dynamic_storage_objects/main.cpp
@@ -1,4 +1,4 @@
-/*
+/**
  storage duration list:
     - static duration ==> (in compile time!) global variables, static local variables in functions, static members in functions
     - automatic duration ==> (in compiler time!) variables in local functions etc.
@@ -13,6 +13,24 @@
               delete expressions(new operators) ==> finish storage duration
   new int, new Myclass, new Ethem, etc. ; variable types ==> int*, Myclass*, Ethem*
 
+ operator new's return value type is equal to malloc at C, malloc failed is null, new failed is exception throw(std::bad_alloc)
+
+ delete is equal to free at C
+
+
+ ATTENTION :
+ ------------
+ void* operator new(size_t t); //new operator calls constructor
+ void  operator delete(void* ); //delete operator calls destructor
+
+ operator new and operator of new are different each other
+ operator delete and operator of delete are different each other
+
+ operator delete and operator new are functions
+ operator of delete and operator of new are operators
+
+ resource leak = if you have used operator of new and haven't used operator of deleted!!!!
+                 Because there is not garbage collector in C++
 
  */
 
